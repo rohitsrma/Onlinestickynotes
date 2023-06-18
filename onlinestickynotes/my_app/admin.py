eb1bfc5f101d0807210db3f4dc250c9934f82ca8
rohitsrma <rohitsrmaofficial@gmail.com>
2023-06-18 23:46:08 +0530

    second commit

--- onlinestickynotes/my_app/admin.py
+++ onlinestickynotes/my_app/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Note
 
-admin.site.register(Note)
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'content', 'timestamp')
+    list_filter = ('user', 'timestamp')
+    search_fields = ('user__username', 'content')
+    date_hierarchy = 'timestamp'
+    ordering = ('-timestamp',)
+    readonly_fields = ('timestamp',)
